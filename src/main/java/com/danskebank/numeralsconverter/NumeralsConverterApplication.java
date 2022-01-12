9537312f7d94a8a8e788dc48be13d7188823f002
RyhorPaprauka <ryhorpaprauka@gmail.com>
2022-01-12 11:24:00 +0300

    logic implemented

---
+++ src/main/java/com/danskebank/numeralsconverter/NumeralsConverterApplication.java
@@ -0,0 +1,13 @@
+package com.danskebank.numeralsconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumeralsConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumeralsConverterApplication.class, args);
+	}
+
+}
